b6cdcfe94324f4dc94d49e0259c3c434c5d3777c
RipFive <richard.poelderl@gmail.com>
2019-12-06 16:26:46 +0100

    added tsconfig.json with rootDir:./src/ therefore created that folder and moved day-one and day-two. Also downloaded tslint

---
+++ src/day-one/script-two.ts
@@ -0,0 +1,20 @@
+import { requiredFuel } from './script-one';
+import { getModules } from './input';
+
+let modules = getModules();
+
+const fuelModules: number[] = modules.map(mass => {
+	let remainder: number = requiredFuel(mass);
+	let totalFuelPerModule: number = requiredFuel(mass);
+	while (remainder > 0) {
+		let additionalFuel = requiredFuel(remainder) > 0 ? requiredFuel(remainder) : 0;
+		totalFuelPerModule += additionalFuel;
+		remainder = additionalFuel;
+	}
+	return totalFuelPerModule;
+});
+
+const totalFuelNeeded: number = fuelModules.reduce((totalFuel, fuel) => {
+	return (totalFuel += fuel);
+});
+console.log('result part two: ' + totalFuelNeeded);
