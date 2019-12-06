9e1b878e89f7a7fa833de0df4fc88bd44cee3eb7
RipFive <richard.poelderl@gmail.com>
2019-12-06 17:19:41 +0100

    fixed minor typos

--- src/day-two/test.ts
+++ src/day-two/test.ts
@@ -8,9 +8,11 @@ export function test() {
 		{ test: [2, 4, 4, 5, 99, 0], answer: [2, 4, 4, 5, 99, 9801] },
 		{ test: [1, 1, 1, 4, 99, 5, 6, 0, 99], answer: [30, 1, 1, 4, 2, 5, 6, 0, 99] }
 	];
-	for (let i in tests) {
-		let test = tests[i]['test'];
-		let answer = tests[i]['answer'];
-		assert.deepEqual(intComputer(test), answer);
+	for (const i in tests) {
+		if (tests.length > 0) {
+			const toTest = tests[i]['test'];
+			const answer = tests[i]['answer'];
+			assert.deepEqual(intComputer(toTest), answer);
+		}
 	}
 }
