965dc83f5dff29cc8683b4717c7ac81d03382518
RipFive <richard.poelderl@gmail.com>
2019-12-09 12:26:04 +0100

    fixed some linting

--- src/day-one/input.ts
+++ src/day-one/input.ts
@@ -100,6 +100,6 @@ export function getModules() {
 138445
 70673`;
 
-	let splittedMasses: string[] = input.split(/\n/);
+	const splittedMasses: string[] = input.split(/\n/).map(numberAsStr => parseInt(numberAsStr, 10));
 	return splittedMasses;
 }
--- src/day-one/script-one.ts
+++ src/day-one/script-one.ts
@@ -1,31 +1,16 @@
 import { getModules } from './input';
 
-const splittedMasses: string[] = getModules();
+const splittedMasses: number[] = getModules();
 
-let fuel: number[] = splittedMasses.map(mass => {
+const fuel: number[] = splittedMasses.map(mass => {
 	return requiredFuel(mass);
 });
 
-let totalFuel: number = fuel.reduce((total, value) => total + value);
+const totalFuel: number = fuel.reduce((total, value) => total + value);
 
 console.log('total fuel required: ' + totalFuel);
 
-export function requiredFuel(mass): number {
-	let mass = mass || 12;
-
-	let firstCalc = Number(mass) / 3;
-	let secondCalc = Math.floor(firstCalc);
-	let thirdCalc = secondCalc - 2;
-
-	return thirdCalc;
-}
-
-function test() {
-	const results = {};
-
-	results['firstTest'] = requiredFuel(12) == 2 ? 'passed' : 'failed';
-	results['secondTest'] = requiredFuel(14) == 2 ? 'passed' : 'failed';
-	results['thirdTest'] = requiredFuel(1969) == 654 ? 'passed' : 'failed';
-	results['fourthTest'] = requiredFuel(100756) == 33583 ? 'passed' : 'failed';
-	return results;
+export function requiredFuel(module: number): number {
+	const calc = Math.floor(module / 3) - 2 || 0;
+	return calc;
 }
