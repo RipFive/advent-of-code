90547d79ecb49090ac775c6757776541e4bbbfed
RipFive <richard.poelderl@gmail.com>
2019-12-10 13:20:24 +0100

    example 1 works, 2 & 3 still buggy (distance is 159, not 157)

--- src/day-three/script-one.ts
+++ src/day-three/script-one.ts
@@ -6,11 +6,19 @@ const wires = readFileSync(inputFilePath)
 	.split(/\n/)
 	.map(wire => wire.split(','));
 
-const example = [
+const example1 = [
 	['R8', 'U5', 'L5', 'D3'],
 	['U7', 'R6', 'D4', 'L4']
 ];
 
+const example2: string[][] = [
+	['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'],
+	['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']
+];
+const example3: string[][] = [
+	['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51'],
+	['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']
+];
 const gridPos: [[number, number], [number, number]] = [
 	[0, 0],
 	[0, 0]
@@ -52,21 +60,22 @@ const getPaths = wires => {
 	return paths;
 };
 
-const paths = getPaths(example);
+const paths = getPaths(example2);
 
-const closestCrossing = (pathObj: {}) => {
+const getDistanceToClosestIntersection = (pathObj: {}) => {
 	const obj = {};
 	const paths = Object.keys(pathObj);
 	for (const path of paths) {
 		if (pathObj[path] > 1) {
 			const posArr = path.split(',');
-			const dist = posArr.map(pos => Number(pos)).reduce((total, increment) => total + increment);
 			if (!obj[path]) {
-				obj[path] = dist;
+				obj[path] = posArr.map(pos => Number(pos)).reduce((total, increment) => total + Math.abs(increment));
 			}
 		}
 	}
-	return Object.keys(obj).reduce((prevValue, curValue) => (obj[prevValue] < obj[curValue] ? prevValue : curValue));
+	const closesIntersection =  Object.keys(obj).reduce((prevDist, curDist) =>
+		obj[prevDist] < obj[curDist] ? prevDist : curDist);
+	return obj[closesIntersection];
 };
 
-console.log(closestCrossing(paths));
+console.log(getDistanceToClosestIntersection(paths));
